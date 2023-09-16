0e4c7c84e4cc59a088de566e4ad2a9984be668c9
Shoeb Ahmed Khan <shoebkhan100100@gmail.com>
2023-09-16 13:25:11 +0530

    Module7-work

--- Module6/Module7/firstwebapp/src/main/java/com/springboot/firstwebapp/Hello/a6Todo/TodoService.java
+++ Module6/Module7/firstwebapp/src/main/java/com/springboot/firstwebapp/Hello/a6Todo/TodoService.java
@@ -1,17 +1,21 @@
 package com.springboot.firstwebapp.Hello.a6Todo;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class TodoService {
-    private static List<a6Todo> todos;
+    private static List<Todo> todos=new ArrayList<>();
     static {
-        todos.add(new a6Todo(1,"Shoeb","Learn AWS", LocalDate.now().plusYears(1),false));
-        todos.add(new a6Todo(2,"Shoeb","Learn Devops",LocalDate.now().plusYears(2),false));
-        todos.add(new a6Todo(3,"Shoeb","Full Stack Development",LocalDate.now().plusYears(3),false));
+        todos.add(new Todo(1,"Shoeb","Learn AWS", LocalDate.now().plusYears(1),false));
+        todos.add(new Todo(2,"Shoeb","Learn Devops",LocalDate.now().plusYears(2),false));
+        todos.add(new Todo(3,"Shoeb","Full Stack Development",LocalDate.now().plusYears(3),false));
     }
 
-    public List<a6Todo> findByUsername(String username){
+    public List<Todo> findByUsername(String username){
         return todos;
     }
 }
--- Module6/Module7/firstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
+++ Module6/Module7/firstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -6,5 +6,6 @@
         <div>Welcome to this page</div>
         <div>Your name: ${name}</div>
         <div>Your password: ${password}</div>
+        <div><a href="list-todos">Manage</a> your todos</div>
     </body>
 </htm
