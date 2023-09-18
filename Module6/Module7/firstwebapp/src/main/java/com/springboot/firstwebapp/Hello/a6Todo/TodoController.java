8c967d37bc5c0eb2d9e83bdfbd6a0059e6207696
Shoeb Ahmed Khan <shoebkhan100100@gmail.com>
2023-09-18 14:57:42 +0530

    Module7-work

--- Module6/Module7/firstwebapp/src/main/java/com/springboot/firstwebapp/Hello/a6Todo/TodoController.java
+++ Module6/Module7/firstwebapp/src/main/java/com/springboot/firstwebapp/Hello/a6Todo/TodoController.java
@@ -3,8 +3,12 @@ package com.springboot.firstwebapp.Hello.a6Todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import java.lang.reflect.Method;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -21,5 +25,17 @@ public class TodoController {
         List<Todo> todos=todoService.findByUsername("Shoeb");
         model.addAttribute("todos",todos);
         return "ListTodos";
+    }
+
+    @RequestMapping(value = "add-todo",method = RequestMethod.GET)
+    public String showNewTodoPage(ModelMap model){
+        return "Todo";
+    }
+
+    @RequestMapping(value = "add-todo",method = RequestMethod.POST)
+    public String addNewTodo(@RequestParam String Description,ModelMap model){
+        String username=(String) model.get("name");
+        todoService.addTodo(username, Description,LocalDate.now().plusYears(1),false);
+        return "redirect:list-todos";
     }
 }
--- Module6/Module7/firstwebapp/src/main/java/com/springboot/firstwebapp/Hello/a6Todo/TodoService.java
+++ Module6/Module7/firstwebapp/src/main/java/com/springboot/firstwebapp/Hello/a6Todo/TodoService.java
@@ -9,13 +9,19 @@ import java.util.List;
 @Service
 public class TodoService {
     private static List<Todo> todos=new ArrayList<>();
+    private static int todoscount=0;
     static {
-        todos.add(new Todo(1,"Shoeb","Learn AWS", LocalDate.now().plusYears(1),false));
-        todos.add(new Todo(2,"Shoeb","Learn Devops",LocalDate.now().plusYears(2),false));
-        todos.add(new Todo(3,"Shoeb","Full Stack Development",LocalDate.now().plusYears(3),false));
+        todos.add(new Todo(++todoscount,"Shoeb","Learn AWS", LocalDate.now().plusYears(1),false));
+        todos.add(new Todo(++todoscount,"Shoeb","Learn Devops",LocalDate.now().plusYears(2),false));
+        todos.add(new Todo(++todoscount,"Shoeb","Full Stack Development",LocalDate.now().plusYears(3),false));
     }
 
     public List<Todo> findByUsername(String username){
         return todos;
+    }
+
+    public void addTodo(String username,String Description,LocalDate TargetDate,boolean isDone){
+        Todo todo=new Todo(++todoscount,username,Description,TargetDate,isDone);
+        todos.add(todo);
     }
 }
